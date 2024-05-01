21a69b5ea3881f75f7a27c15eb95ef522fedbf7d
RobertoBaiochi <robertombbaiocho@gmail.com>
2024-04-30 23:28:24 -0300

    feat: create insert migration in project

---
+++ src/migration/1714529072714-create_table_city.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableCity1714529072714 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+    CREATE TABLE public.city (
+        id int NOT NULL,
+        state_id int NOT NULL,
+        name character varying NOT NULL,
+        created_at timestamp without time zone DEFAULT now() NOT NULL,
+        update_ad timestamp without time zone DEFAULT now() NOT NULL,
+        primary key (id),
+        foreign key (state_id) references public.state(id) 
+    );
+    
+    CREATE SEQUENCE public.city_id_seq
+        AS int
+        START WITH 1
+        INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1;
+    
+    ALTER SEQUENCE public.city_id_seq OWNED BY public.city.id;
+    
+    ALTER TABLE ONLY public.city ALTER COLUMN id SET DEFAULT nextval('public.city_id_seq'::regclass);
+        
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        drop table public.city;
+    `);
+  }
+}
---
+++ src/migration/1714530060960-create_table_address.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableAddress1714530060960 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        CREATE TABLE public.address (
+            id int NOT NULL,
+            user_id int NOT NULL,
+            complement character varying NOT NULL,
+            number int NOT NULL,
+            cep character varying NOT NULL,
+            city_id int NOT NULL,
+            created_at timestamp without time zone DEFAULT now() NOT NULL,
+            update_ad timestamp without time zone DEFAULT now() NOT NULL,
+            primary key (id),
+            foreign key (user_id) references public.user(id),
+            foreign key (city_id) references public.city(id)
+        );
+
+        CREATE SEQUENCE public.address_id_seq
+            AS int
+            START WITH 1
+            INCREMENT BY 1
+            NO MINVALUE
+            NO MAXVALUE
+            CACHE 1;
+
+        ALTER SEQUENCE public.address_id_seq OWNED BY public.address.id;
+
+        ALTER TABLE ONLY public.address ALTER COLUMN id SET DEFAULT nextval('public.address_id_seq'::regclass);
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`drop table public.address`);
+  }
+}
