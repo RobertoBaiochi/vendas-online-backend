2565f3413a32145e8a4d575babfd6fccd7d179d3
RobertoBaiochi <robertombbaiocho@gmail.com>
2024-05-01 22:49:45 -0300

    feat: insert validation address

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -8,10 +8,16 @@ import {
 } from '@nestjs/common';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
+import { UserService } from 'src/user/user.service';
+import { CityService } from 'src/city/city.service';
 
 @Controller('address')
 export class AddressController {
-  constructor(private addressService: AddressService) {}
+  constructor(
+    private addressService: AddressService,
+    private userService: UserService,
+    private cityService: CityService,
+  ) {}
 
   @Post('/:userId')
   @UsePipes(ValidationPipe)
@@ -19,6 +25,10 @@ export class AddressController {
     @Body() createAddresDto: CreateAddressDto,
     @Param('userId') userId: number,
   ) {
+    await this.userService.findUserById(userId);
+
+    await this.cityService.findCityById(createAddresDto.cityId);
+
     return this.addressService.createAddress(createAddresDto, userId);
   }
 }
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
 import { Repository } from 'typeorm';
@@ -24,5 +24,19 @@ export class CityService {
 
   async getAllCities(): Promise<CityEntity[]> {
     return this.cityRepository.find();
+  }
+
+  async findCityById(cityId: number): Promise<CityEntity> {
+    const city = await this.cityRepository.findOne({
+      where: {
+        id: cityId,
+      },
+    });
+
+    if (!city) {
+      throw new NotFoundException('cityId not found');
+    }
+
+    return city;
   }
 }
